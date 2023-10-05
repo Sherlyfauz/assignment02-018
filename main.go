469c0bd966057ddc183bf355cf1df62d1563d156
Sherly Fauziyah Syaharani <85879078+Sherlyfauz@users.noreply.github.com>
2023-10-05 16:52:16 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"assignment-2/database"
+	"assignment-2/routers"
+)
+
+func main() {
+	PORT := ":8080"
+
+	_, err := database.InitDB()
+
+	if err != nil {
+		panic(err)
+	}
+
+	database.Migrate()
+
+	routers.SetupRouter().Run(PORT)
+
+}
